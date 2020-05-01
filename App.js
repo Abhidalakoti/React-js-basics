10dc2c5f61bd613b360c0dce029335d1fc3c5ea6
Abhinav Dalakoti <abhinavdalakoti@gmail.com>
2020-05-01 23:06:49 +0530

    New Commit

--- App.js
+++ App.js
@@ -1,32 +1,26 @@
 import React from 'react';
+class App extends React.Component{
 
-class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <Header/>
-            <Content/>
-         </div>
-      );
-   }
+   constructor(props) {
+      super(props);
+   
+   this.state = {
+
+      header: "Header from state...",
+      content: "Content from state..."
+   }}
+
+
+render() {
+   return (
+   <div>
+
+      <h1>{this.state.header}</h1>
+     <h2> {this.state.content}</h2>
+      </div>
+   );
+
 }
-class Header extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Header</h1>
-         </div>
-      );
-   }
-}
-class Content extends React.Component {
-   render() {
-      return (
-         <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
-         </div>
-      );
-   }
+
 }
 export default App;
