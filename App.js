c506bca58da2c78f36db1887498a1903165fcd04
Abhinav Dalakoti <abhinavdalakoti@gmail.com>
2020-05-02 15:50:25 +0530

    Default Props

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+iimport React from 'react';
 
 class App extends React.Component {
    render() {
-
       return (
          <div>
-
-      <h1> {this.props.headerProp}</h1>
-      <h2> {this.props.contentProp}</h2>
+            <h1>{this.props.headerProp}</h1>
+            <h2>{this.props.contentProp}</h2>
          </div>
-
       );
-
-
    }
-
 }
+App.defaultProps = {
+   headerProp: "Header from props...",
+   contentProp:"Content from props..."
+}
+export default App;
 
    App.defaultProps = {
-      headerProp: "This is heaer Prop....",
+      headerProp: "This is header Prop....",
       contentProp: "This is content prop..."
    }
 
