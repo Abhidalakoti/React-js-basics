8db98718d0862960656b38265fea8b7387f4edb3
Abhinav Dalakoti <abhinavdalakoti@gmail.com>
2020-05-03 15:34:18 +0530

    remove App.jsx

--- App.js
+++ App.js
@@ -1,42 +1,58 @@
-import React from 'react';
-
-class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <h3>Array: {this.props.propArray}</h3>
-            <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
-            <h3>Func: {this.props.propFunc(3)}</h3>
-            <h3>Number: {this.props.propNumber}</h3>
-            <h3>String: {this.props.propString}</h3>
-            <h3>Object: {this.props.propObject.objectName1}</h3>
-            <h3>Object: {this.props.propObject.objectName2}</h3>
-            <h3>Object: {this.props.propObject.objectName3}</h3>
-         </div>
-      );
-   }
-}
-
-App.propTypes = {
-   propArray: React.PropTypes.array.isRequired,
-   propBool: React.PropTypes.bool.isRequired,
-   propFunc: React.PropTypes.func,
-   propNumber: React.PropTypes.number,
-   propString: React.PropTypes.string,
-   propObject: React.PropTypes.object
-}
-
-App.defaultProps = {
-   propArray: [1,2,3,4,5],
-   propBool: true,
-   propFunc: function(e){return e},
-   propNumber: 1,
-   propString: "String value...",
-   
-   propObject: {
-      objectName1:"objectValue1",
-      objectName2: "objectValue2",
-      objectName3: "objectValue3"
-   }
-}
+import React, { Component } from 'react';  
+import PropTypes from 'prop-types';  
+class App extends React.Component {  
+   render() {  
+      return (  
+          <div>  
+              <h1>ReactJS Props validation example</h1>  
+              <table>  
+                  <tr>  
+                      <th>Type</th>  
+                      <th>Value</th>  
+                      <th>Valid</th>  
+                  </tr>  
+                <tr>  
+                      <td>Array</td>  
+                      <td>{this.props.propArray}</td>  
+                      <td>{this.props.propArray ? "true" : "False"}</td>  
+                  </tr>  
+                  <tr>  
+                      <td>Boolean</td>  
+                      <td>{this.props.propBool ? "true" : "False"}</td>  
+                      <td>{this.props.propBool ? "true" : "False"}</td>  
+                  </tr>  
+                  <tr>  
+                      <td>Function</td>  
+                      <td>{this.props.propFunc(5)}</td>  
+                      <td>{this.props.propFunc(5) ? "true" : "False"}</td>  
+                  </tr>  
+                  <tr>  
+                      <td>String</td>  
+                      <td>{this.props.propString}</td>  
+                      <td>{this.props.propString ? "true" : "False"}</td>  
+                  </tr>  
+                  <tr>  
+                      <td>Number</td>  
+                      <td>{this.props.propNumber}</td>  
+                      <td>{this.props.propNumber ? "true" : "False"}</td>  
+                  </tr>  
+             </table>  
+        </div>  
+        );  
+   }  
+}  
+App.propTypes = {  
+    propArray: PropTypes.array.isRequired,  
+    propBool: PropTypes.bool.isRequired,  
+    propFunc: PropTypes.func,  
+    propNumber: PropTypes.number,  
+    propString: PropTypes.string,   
+}  
+App.defaultProps = {  
+    propArray: [1,2,3,4,5],  
+    propBool: true,  
+    propFunc: function(x){return x+5},  
+    propNumber: 1,  
+    propString: "JavaTpoint",  
+}  
 export default App;
